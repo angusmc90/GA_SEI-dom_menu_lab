2bd097a92013f984bdf3d3f1e3fbd25e6afd7ed3
angusmc90 <angusmc90@gmail.com>
2023-10-01 21:55:51 -0500

    update menuLinks array

--- js/app.js
+++ js/app.js
@@ -3,9 +3,20 @@ console.log("DOM Lab Homework - Angus")
 // Menu data structure
 const menuLinks = [
     {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
   ];
 
 const mainEl = document.querySelector('main')
